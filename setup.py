62739a4e752ecb2278a21679e5c765d6fcb4cace
Manmeet Singh <manmeet20singh11@gmail.com>
2024-12-14 16:39:00 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
+# Read dependencies from requirements.txt
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 setup(
     name="DeepAssimilate",
     version="0.1.0",
-    description="Deep learning framework for assimilating data with deep learning.",
+    description="Deep learning framework for assimilating data with SRCNN.",
     author="Your Name",
     author_email="your.email@example.com",
     packages=find_packages(),
-    install_requires=[
-        "torch>=1.7.1",
-        "numpy",
-        "opencv-python",
-        "scikit-learn"
-    ],
+    install_requires=required,  # Use the dependencies from requirements.txt
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
